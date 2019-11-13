003ccaa0756f173077e04192aaba6b88d70e641e
ozyz <lucaszyzelewicz@gmail.com>
2019-11-13 15:37:34 +0100

    Added comments

--- circuit.h
+++ circuit.h
@@ -11,18 +11,18 @@ using namespace std;
 
 class Circuit{
 private:
-  string name;
-  string dotFile;
-  map<string, Node> circuitInputs;
-  map<string, Node> circuitOutputs;
-  map<string, Node> circuitGates;
+  string name;    //Circuit name
+  string dotFile; //Dotfile path
+  map<string, Node> circuitInputs;  //Map of the circuit inputs
+  map<string, Node> circuitOutputs; //Map of the circuit outputs
+  map<string, Node> circuitGates;   //Map of the circuit gates
 public:
-  Circuit(const string & nom, const string & dotFile);
-  ~Circuit(){;}
+  Circuit(const string & nom, const string & dotFile);  //Constructor
+  ~Circuit(){;}                                         //Destructor
 
-  parse();
-  checkSumDelta();
-  evaluate();
+  parse();          //Parses the dot file and creates the circuit inputs, outputs and gates with the link between them.
+  checkSumDelta();  //Checks the delta of all the circuit nodes.
+  evaluate();       //Runs the simulation of the circuit 1 time, sets the outputs.
 };
 
 #endif
--- node.h
+++ node.h
@@ -10,17 +10,17 @@ using namespace std;
 
 class Node : public Circuit{
 private:
-  string name;
-  string type;
-  bool delta;
-  bool output;
-  map<string, Node> inputs;
-  map<string, Node> outputs;
+  string name;  //Name of the node
+  string type;  //Type of the node (INPUT, OUTPUT, AND_2, NOT...)
+  bool delta;   //Delta of the node
+  bool output;  //Output of the node, calcultated by "computeOutput()" method.
+  map<string, Node> inputs;   //Map of the node inputs
+  map<string, Node> outputs;  //Map of the node outputs
 public:
-  Node(const string & nom, const string & type);
-  ~Node(){;}
+  Node(const string & nom, const string & type);  //Constructor
+  ~Node(){;}                                      //Destructor
 
-  void computeOutput();
-  void checkDelta();
+  void computeOutput(); //Calculate the output from the inputs and node type
+  void checkDelta(); //Check if the deltas of the inputs = 1
 };
 #endif
