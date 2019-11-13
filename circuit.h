5da84b0dbbc164fea25b0d425ba3818df8269904
ozyz <lucaszyzelewicz@gmail.com>
2019-11-13 15:16:38 +0100

    Base

--- circuit.h
+++ circuit.h
@@ -1,17 +1,28 @@
+#ifndef DEF_CIRCUIT
+#define DEF_CIRCUIT
+
+
 #include <string>
 #include <iostream>
+#include <map>
+#include "node.h"
 
 using namespace std;
 
 class Circuit{
 private:
-  string m_nom;
-  int m_nb_inputs;
-  int m_nb_outputs;
-  int m_inputs[];
-  int m_outputs[];
-
+  string name;
+  string dotFile;
+  map<string, Node> circuitInputs;
+  map<string, Node> circuitOutputs;
+  map<string, Node> circuitGates;
 public:
-  Circuit(const string & nom);
+  Circuit(const string & nom, const string & dotFile);
   ~Circuit(){;}
+
+  parse();
+  checkSumDelta();
+  evaluate();
 };
+
+#endif
--- node.h
+++ node.h
@@ -1,16 +1,26 @@
+#ifndef DEF_NODE
+#define DEF_NODE
+
+
 #include <string>
 #include <iostream>
 #include <map>
+#include "circuit.h"
 using namespace std;
 
-class Node{
+class Node : public Circuit{
 private:
   string name;
   string type;
   bool delta;
   bool output;
   map<string, Node> inputs;
+  map<string, Node> outputs;
 public:
-  Node(const string & nom);
+  Node(const string & nom, const string & type);
   ~Node(){;}
+
+  void computeOutput();
+  void checkDelta();
 };
+#endif
