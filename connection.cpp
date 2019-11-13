39c30a7d50c1ba4de4e4694ad01f5421eb1b6155
ozyz <lucaszyzelewicz@gmail.com>
2019-11-13 14:04:55 +0100

    Deleted conection class

--- connection.cpp
+++
@@ -1,5 +0,0 @@
-#include "connection.h"
-#include <string>
-#include <iostream>
-
-using namespace std;
--- node.h
+++ node.h
@@ -5,7 +5,12 @@ using namespace std;
 
 class Node{
 private:
-
+  string name;
+  string type;
+  bool delta;
+  bool output;
+  map<string, *Node> inputs;
 public:
-
+  Node(const string & nom);
+  ~Node(){;}
 };
