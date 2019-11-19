63266d80e8586d18e33fc5fcd2e8c8c4c971a44c
ozyz <lucaszyzelewicz@gmail.com>
2019-11-19 14:56:51 +0100

    Node

--- circuit.cpp
+++ circuit.cpp
@@ -1,9 +1,18 @@
 #include "circuit.h"
 #include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-Circuit::Circuit(const string & nom): m_nom(nom){
-  
+Circuit::Circuit(const string & nom, const string & dotFile): m_name(nom), m_dotFile(dotFile){
+
+}
+
+void Circuit::parse(){
+  ifstream is (m_dotFile);
+  string str;
+  while (getline(is, str)) {
+    cout << "str" << '\n';
+  }
 }
--- node.h
+++ node.h
@@ -5,17 +5,15 @@
 #include <string>
 #include <iostream>
 #include <map>
-#include "circuit.h"
 
 using namespace std;
 
-<<<<<<< HEAD
-class Node : public Circuit {
+class Node {
 
 private:
 
   //Déclaration des variables
-  string name;
+  string m_name;
   string type;
   bool delta;
   bool output;
@@ -25,8 +23,7 @@ private:
 public:
 
   //Constructeur de la classe Node
-  Node(const string & nom) :
-  Circuit(nom){
+  Node(const string & nom):m_name(nom){
     cout << "Constructeur de la classe Node" << endl;
   }
 
@@ -41,21 +38,5 @@ public:
   //Méthodes
   void computeOutput();
   void checkInputDelta();
-
-=======
-class Node : public Circuit{
-private:
-  string name;  //Name of the node
-  string type;  //Type of the node (INPUT, OUTPUT, AND_2, NOT...)
-  bool delta;   //Delta of the node
-  bool output;  //Output of the node, calcultated by "computeOutput()" method.
-  map<string, Node> inputs;   //Map of the node inputs
-  map<string, Node> outputs;  //Map of the node outputs
-public:
-  Node(const string & nom, const string & type);  //Constructor
-  ~Node(){;}                                      //Destructor
-  void computeOutput(); //Calculate the output from the inputs and node type
-  void checkDelta(); //Check if the deltas of the inputs = 1
->>>>>>> 003ccaa0756f173077e04192aaba6b88d70e641e
 };
 #endif
