dac29bae2aaa890e8319e350eac4b61f3f5fda39
SVVAYZ <57670011+SVVAYZ@users.noreply.github.com>
2019-11-19 14:15:43 +0100

    Node V1

--- node.h
+++ node.h
@@ -3,14 +3,36 @@
 #include <map>
 using namespace std;
 
-class Node{
+class Node : public Circuit {
+
 private:
+
+  //Déclaration des variables
   string name;
   string type;
   bool delta;
   bool output;
   map<string, Node> inputs;
+  map<string, Node> outputs;
+
 public:
-  Node(const string & nom);
-  ~Node(){;}
+
+  //Constructeur de la classe Node
+  Node(const string & nom) :
+  Circuit(nom){
+    cout << "Constructeur de la classe Node" << endl;
+  }
+
+  //Destructeur de la classe Node
+  ~Node(){
+    cout << "Destructeur de la classe Node" << endl;
+  };
+
+  //Modifieurs et accesseurs
+
+
+  //Méthodes
+  void computeOutput();
+  void checkInputDelta();
+
 };
